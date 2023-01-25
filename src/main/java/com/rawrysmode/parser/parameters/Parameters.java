dc8729fc897a422f06952dcd95073c2155ec2731
RawrysMode <iamisaacmu@gmail.com>
2023-01-25 16:56:30 +0700

    Update Parameters.java

--- src/main/java/com/rawrysmode/parser/parameters/Parameters.java
+++ src/main/java/com/rawrysmode/parser/parameters/Parameters.java
@@ -1,17 +1,22 @@
 package com.rawrysmode.parser.parameters;
 
+import com.rawrysmode.utilities.Deserializer;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
 public class Parameters {
 
     private final Ordering ordering;
     private final Type type;
     private final String outputFilename;
-    private final String[] inputFilenames;
+    private final ArrayList<BufferedReader> bufferedReaders;
 
     public Parameters(String ordering, String type, String outputFilename, String inputFilenames) {
         this.ordering = ordering.equals("-a") ? Ordering.ASCENDING : Ordering.DESCENDING;
         this.type = type.equals("-i") ? Type.INTEGER : Type.STRING;
         this.outputFilename = outputFilename;
-        this.inputFilenames = inputFilenames.split(" ");
+        this.bufferedReaders = Deserializer.initializeBufferedReaders(inputFilenames.split(" "));
     }
 
     public Ordering getOrdering() {
@@ -26,7 +31,7 @@ public class Parameters {
         return outputFilename;
     }
 
-    public String[] getInputFilenames() {
-        return inputFilenames;
+    public ArrayList<BufferedReader> getInputFilenames() {
+        return bufferedReaders;
     }
 }
