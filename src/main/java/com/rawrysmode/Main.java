03a9c62faa0f6ee79716ad616e24c7059a83fe32
RawrysMode <iamisaacmu@gmail.com>
2023-01-25 16:07:54 +0700

    Initial tests

--- src/main/java/com/rawrysmode/Main.java
+++ src/main/java/com/rawrysmode/Main.java
@@ -1,5 +1,7 @@
-package com.rawrysmode;public class Main {
+package com.rawrysmode;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
